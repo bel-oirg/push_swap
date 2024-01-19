855889889579336fe7b3140f097bd3b936362afe
Badr Eddine El Oirghi <bel-oirg@e1r11p1.1337.ma>
2024-01-19 23:48:38 +0100

    added header

---
+++ checker_bonus.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checker_bonus.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bel-oirg <bel-oirg@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/05 17:58:50 by bel-oirg          #+#    #+#             */
+/*   Updated: 2024/01/19 23:48:22 by bel-oirg         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CHECKER_BONUS_H
+# define CHECKER_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4
+# endif
+
+# include "../push_swap/push_swap.h"
+
+//next_line
+void	free_out(char *out_free);
+char	*ft_strjoin_gnl(char *s1, char *s2);
+int		have_n(char	*temp);
+char	*print_line(char **temp);
+char	*get_next_line(int fd);
+
+//strcmp
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
+
