28bb7918bc2f5353e216ef47d89da51cc36d6a85
Badr Eddine El Oirghi <bel-oirg@e1r2p14.1337.ma>
2024-01-20 04:18:30 +0100

    better checker

--- checker_bonus.c
+++ checker_bonus.c
@@ -6,37 +6,51 @@
 /*   By: bel-oirg <bel-oirg@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 23:46:49 by bel-oirg          #+#    #+#             */
-/*   Updated: 2024/01/19 23:59:04 by bel-oirg         ###   ########.fr       */
+/*   Updated: 2024/01/20 04:14:04 by bel-oirg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "checker_bonus.h"
 
-int	atoc(char *command, t_stack_node **a, t_stack_node **b)
+static int atoc1(char *command, t_stack_node **a, t_stack_node **b)
 {
 	if (!ft_strcmp(command, "ra\n"))
-		ra(a, 1);
+		(lst_len(*a) >= 2) && (ra(a, 1), 0);
 	else if (!ft_strcmp(command, "rb\n"))
-		rb(b, 1);
+		(lst_len(*b) >= 2) && (rb(b, 1), 0);
 	else if (!ft_strcmp(command, "rra\n"))
-		rra(a, 1);
-	else if (!ft_strcmp(command, "rrb\n"))
-		rrb(b, 1);
+		(lst_len(*a) >= 2) && (rra(a, 1), 0);
 	else if (!ft_strcmp(command, "pa\n"))
-		pa(a, b, 1);
+		(lst_len(*b) >= 1) && (pa(a, b, 1), 0);
 	else if (!ft_strcmp(command, "pb\n"))
-		pb(b, a, 1);
+		(lst_len(*a) >= 1) && (pb(b, a, 1), 0);
 	else if (!ft_strcmp(command, "sa\n"))
-		sa(a, 1);
-	else if (!ft_strcmp(command, "sb\n"))
-		sb(b, 1);
-	else if (!ft_strcmp(command, "rrr\n"))
-		rrr(a, b, 1);
-	else if (!ft_strcmp(command, "rr\n"))
-		rr(a, b, 1);
-	else if (!ft_strcmp(command, "ss\n"))
-		ss(a, b, 1);
+		(lst_len(*a) >= 2) && (sa(a, 1), 0);
 	else
+		return (0);
+	return (-1);
+}
+
+static int	atoc2(char *command, t_stack_node **a, t_stack_node **b)
+{
+	if (!ft_strcmp(command, "sb\n"))
+		(lst_len(*b) >= 2) && (sb(b, 1), 0);
+	else if (!ft_strcmp(command, "rrr\n"))
+		((lst_len(*a) >= 2) && (lst_len(*b) >= 2) && (rrr(a, b, 1), 0));
+	else if (!ft_strcmp(command, "rr\n"))
+		((lst_len(*a) >= 2) && (lst_len(*b) >= 2) && (rr(a, b, 1), 0));
+	else if (!ft_strcmp(command, "rrb\n"))
+		(lst_len(*b) >= 2) && (rrb(b, 1), 0);
+	else if (!ft_strcmp(command, "ss\n"))
+		(lst_len(*a) >= 2) && (lst_len(*b) >= 2) && (ss(a, b, 1), 0);
+	else
+		return (0);
+	return (-1);
+}
+
+static int	checker_controller(char *command, t_stack_node **a, t_stack_node **b)
+{
+	if ((!atoc1(command, a, b)) && (!atoc2(command, a, b)))
 		return (1);
 	return (0);
 }
@@ -51,14 +65,14 @@ int	main(int argc, char *argv[])
 		return (0);
 	if (argc >= 2)
 		argv = parsed(argv, argc);
-	if (init_stack(&a, argv))
+	if (init_stack(&a, argv) == 1)
 		return (write(2, "Error\n", 6), my_malloc(0, 0), 0);
 	while (1)
 	{
 		command = get_next_line(0);
 		if (!command)
 			break ;
-		if (atoc(command, &a, &b))
+		if (checker_controller(command, &a, &b))
 			(free(command), write(2, ERR, 6), my_malloc(0, 0));
 		free(command);
 	}
--- my_malloc.c
+++ my_malloc.c
@@ -6,13 +6,13 @@
 /*   By: bel-oirg <bel-oirg@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/05 01:37:28 by bel-oirg          #+#    #+#             */
-/*   Updated: 2024/01/19 18:07:01 by bel-oirg         ###   ########.fr       */
+/*   Updated: 2024/01/20 04:15:41 by bel-oirg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	dataclear(t_data **hold)
+void	lst_free(t_data **hold)
 {
 	t_data	*tmp;
 	t_data	*ptr;
@@ -40,10 +40,10 @@ void	*my_malloc(size_t size, int mode)
 	{
 		data = malloc(size);
 		if (!data)
-			return (dataclear(&head), exit(1), NULL);
+			return (lst_free(&head), exit(1), NULL);
 		node = malloc(sizeof(t_data));
 		if (!node)
-			return (dataclear(&head), exit(1), NULL);
+			return (lst_free(&head), exit(1), NULL);
 		node->data = data;
 		node->next = NULL;
 		(head) && (node->next = head);
@@ -51,6 +51,6 @@ void	*my_malloc(size_t size, int mode)
 		return (data);
 	}
 	if (!mode)
-		return (dataclear(&head), exit(1), NULL);
-	return (dataclear(&head), exit(0), NULL);
+		return (lst_free(&head), exit(1), NULL);
+	return (lst_free(&head), exit(0), NULL);
 }
